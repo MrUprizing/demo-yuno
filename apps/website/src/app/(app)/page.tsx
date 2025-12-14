a1c62b801c54897034f01e9b5dc94c0809f5874f
Mr uprizing <uprizingfaze@gmail.com>
2025-12-14 15:22:24 -0500

    Fix: Links landing page

--- apps/website/src/app/(app)/page.tsx
+++ apps/website/src/app/(app)/page.tsx
@@ -41,10 +41,12 @@ export default function HeroSection() {
             <div className="text-center sm:mx-auto lg:mr-auto lg:mt-0">
               <AnimatedGroup variants={transitionVariants}>
                 <Link
-                  href="#link"
+                  href="https://y.uno"
                   className="hover:bg-background dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 shadow-md shadow-zinc-950/5 transition-colors duration-300 dark:border-t-white/5 dark:shadow-zinc-950"
                 >
-                  <span className="text-foreground text-sm">Yuno x AI</span>
+                  <span className="text-foreground text-sm">
+                    Hackathon Yuno
+                  </span>
                   <span className="dark:border-background block h-4 w-0.5 border-l bg-white dark:bg-zinc-700"></span>
 
                   <div className="bg-background group-hover:bg-muted size-6 overflow-hidden rounded-full duration-500">
@@ -103,7 +105,7 @@ export default function HeroSection() {
                     size="lg"
                     className="rounded-xl px-5 text-base"
                   >
-                    <Link href="#link">
+                    <Link href="/playground">
                       <span className="text-nowrap">Start Building</span>
                     </Link>
                   </Button>
@@ -115,8 +117,8 @@ export default function HeroSection() {
                   variant="ghost"
                   className="h-10.5 rounded-xl px-5"
                 >
-                  <Link href="#link">
-                    <span className="text-nowrap">Request a demo</span>
+                  <Link href="https://github.com/MrUprizing/demo-yuno">
+                    <span className="text-nowrap">GitHub</span>
                   </Link>
                 </Button>
               </AnimatedGroup>
