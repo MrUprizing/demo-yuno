8899c0c14c36c4ace29730dd2e50f284621e86b6
Mr uprizing <uprizingfaze@gmail.com>
2025-12-14 10:46:31 -0500

    Fix: Sdk

---
+++ packages/yuno-demo-sdk-ui/src/index.d.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface YunoIframeProps {
+  url: string;
+  width?: string;
+  height?: string;
+}
+
+export function YunoIframe(props: YunoIframeProps): React.ReactElement;
+
+export default YunoIframe;
