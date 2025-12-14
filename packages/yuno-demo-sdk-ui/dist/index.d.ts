605b767864b04bda9958dcab9feb8864a4d8d29e
Mr uprizing <uprizingfaze@gmail.com>
2025-12-14 10:39:24 -0500

    Stop tracking node_modules and dist folders

--- packages/yuno-demo-sdk-ui/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export interface YunoIframeProps {
-    url: string;
-    width?: string;
-    height?: string;
-}
-export declare function YunoIframe({ url, width, height, }: YunoIframeProps): import("react/jsx-runtime").JSX.Element;
-export default YunoIframe;
