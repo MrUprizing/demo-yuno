888677bbd66eaf641fe2ee88be719ffeaf295d65
Mr uprizing <uprizingfaze@gmail.com>
2025-12-14 13:20:15 -0500

    Fix: localhost

--- apps/website/src/app/(app)/frame/page.tsx
+++ apps/website/src/app/(app)/frame/page.tsx
@@ -5,7 +5,7 @@ import IFrame from "react-iframe";
 export default function FramePage() {
   return (
     <div>
-      <IFrame url="http://localhost:3000/test" width="100%" height="600px" />
+      <IFrame url="https://yuno.uprizing.me/test" width="100%" height="600px" />
     </div>
   );
 }
